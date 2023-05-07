3958cab9112f87feb6891a693500fba9ffc00c3d
Kenkentake <keeeentake0908@gmail.com>
2023-05-07 16:31:43 +0900

    add s3 app

---
+++ serverless/s3/app.py
@@ -0,0 +1,31 @@
+from constructs import Construct
+import aws_cdk as cdk
+from aws_cdk import (
+    Stack,
+    aws_s3 as s3
+)
+import os
+
+
+class SimpleS3(Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # S3 bucket to store data
+        bucket = s3.Bucket(
+            self, "bucket",
+            removal_policy=cdk.RemovalPolicy.DESTROY,
+            auto_delete_objects=True,
+        )
+
+        cdk.CfnOutput(self, "BucketName", value=bucket.bucket_name)
+
+app = cdk.App()
+SimpleS3(
+    app, "SimpleS3",
+    env={
+        "region": os.getenv("CDK_DEFAULT_REGION"),
+        "account": os.getenv("CDK_DEFAULT_ACCOUNT"),
+    }
+)
+app.synth()
